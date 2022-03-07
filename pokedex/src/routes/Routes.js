7e740720b72a274f2e551537529e8df5af313cc2
andreyna1808 <andreyna.m.carvalho@gmail.com>
2022-03-07 14:49:33 -0400

    Básico das paginas

---
+++ pokedex/src/routes/Routes.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ErrorPage from "../pages/error/error";
+import Home from "../pages/home/home";
+import Pokedex from "../pages/pokedex/pokedex";
+import PokedexDetails from "../pages/pokedexDetail/pokedexDetail";
+
+export default function Rotas () {
+
+  return <div>
+    
+			<BrowserRouter>
+				<Routes>
+					<Route exact path="/" element={<Home/>} />
+					<Route exact path="/pokedex" element={<Pokedex/>} />
+					<Route exact path="/pokedex-detail" element={<PokedexDetails/>} />
+					<Route exact path="*" element={<ErrorPage />} />
+
+				</Routes>
+			</BrowserRouter>
+
+  </div>
+}
+
