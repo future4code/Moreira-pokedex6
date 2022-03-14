9c15b1078ec77bf2faf899e354e3d4ed6fca8f0b
Larissa97-fir <larissafirmino097@gmail.com>
2022-03-13 22:02:51 -0300

    Ataulizando

--- src/pages/pokedexDetail/pokedexDetail.js
+++ src/pages/pokedexDetail/pokedexDetail.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import HeaderDetails from '../../components/HeaderDetails';
 import {
 	ContainerDetails,
@@ -8,24 +9,25 @@ import {
 	ContainerPoderes,
 	ContainerAtaques,
 	ContainerTipoPokemon,
-  ImgPoke,
-  Power
+	ImgPoke,
+	Power
 } from './style.js';
 export default function PokedexDetails(props) {
-	const [ tipe, setTipe ] = useState([]);
-  const [ imgPokemon, setImgPokemon ] = useState([])
-  const [ moves, setMoves ] = useState([])
-  const [ power, setPower ] = useState([])
-	const name = localStorage.getItem('namePokemon');
+	const [tipe, setTipe] = useState([]);
+	const [imgPokemon, setImgPokemon] = useState([])
+	const [moves, setMoves] = useState([])
+	const [power, setPower] = useState([])
+
+	const { name } = useParams()
 
 	const getInfoPokemon = () => {
 		axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
-    .then((res) => {
-			setTipe(res.data.types);
-      setImgPokemon(res.data.sprites)
-      setMoves(res.data.moves)
-      setPower(res.data.stats)
-		});
+			.then((res) => {
+				setTipe(res.data.types);
+				setImgPokemon(res.data.sprites)
+				setMoves(res.data.moves)
+				setPower(res.data.stats)
+			});
 	};
 
 	useEffect(() => {
@@ -38,39 +40,39 @@ export default function PokedexDetails(props) {
 			<ContainerDetails>
 				<div>
 					<CardFoto>
-						<ImgPoke src={imgPokemon.front_default}/>
+						<ImgPoke src={imgPokemon.front_default} />
 					</CardFoto>
 					<CardFoto>
-						<ImgPoke src={imgPokemon.back_default}/>
+						<ImgPoke src={imgPokemon.back_default} />
 					</CardFoto>
 				</div>
 				<ContainerPoderes>
-        <h2>Poderes</h2>
-        {power.map((dados) => {
-          return <Power key={dados.id}>
-            <p>{dados.stat.name}</p>
-            <p>{dados.base_stat}</p>
-          </Power>
-        })}
+					<h2>Poderes</h2>
+					{power.map((dados) => {
+						return <Power key={dados.id}>
+							<p>{dados.stat.name}</p>
+							<p>{dados.base_stat}</p>
+						</Power>
+					})}
 				</ContainerPoderes>
 				<div>
 					<ContainerTipo>
 						<h2>Tipo</h2>
 						<ContainerTipoPokemon>
-              {tipe.map((dados) => {
-                return <div key={dados.id}>
-                  <p>{dados.type.name}</p>
-                </div>
-              })}
+							{tipe.map((dados) => {
+								return <div key={dados.id}>
+									<p>{dados.type.name}</p>
+								</div>
+							})}
 						</ContainerTipoPokemon>
 					</ContainerTipo>
 					<ContainerAtaques>
 						<h2>Principais ataques</h2>
-            {moves.slice(0,5).map((dados) => {
-            return <div key={dados.id}>
-              <p>{dados.move.name}</p>
-            </div>
-          })}
+						{moves.slice(0, 5).map((dados) => {
+							return <div key={dados.id}>
+								<p>{dados.move.name}</p>
+							</div>
+						})}
 					</ContainerAtaques>
 				</div>
 			</ContainerDetails>
